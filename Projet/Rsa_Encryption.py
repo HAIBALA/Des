f091e608c65101fa8998d421d204bd2e2acbfb23
MEQQORI <meqqorikhalil@gmail.com>
2019-03-14 23:33:39 +0100

    Amine commit

--- Projet/Rsa_Encryption.py
+++ Projet/Rsa_Encryption.py
@@ -8,7 +8,7 @@ key = RSA.generate(1024, random_generator) #generate pub and priv key
 
 publickey = key.publickey() # pub key export for exchange
 
-encrypted = publickey.encrypt('encrypt this message', 32)
+encrypted = publickey.encrypt('message message', 32)
 #message to encrypt is in the above line 'encrypt this message'
 
 print 'encrypted message:', encrypted #ciphertext
